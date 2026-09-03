b3650662477a8ae99e22de85d07b6c21b252453c
Kenas_KL <kenaskili@gmail.com>
2026-09-04 00:22:00 +0200

    added environement variables

--- config/settings.py
+++ config/settings.py
@@ -11,23 +11,32 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+env = environ.Env(
+    DEBUG=(bool, False),
+    ALLOWED_HOSTS=(str, ''),
+    SECRET_KEY=(str, ''),
+)
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-blj1(+d8!0bk5v9xw3$cz(+n5&%%vory-6mfu6@y*wnp8++0^4'
 
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = [
-    '*'
-]
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
 
 
 # Application definition
@@ -130,9 +139,6 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles',
-]
 
 
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
